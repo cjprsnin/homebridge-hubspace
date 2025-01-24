730715295b857a25823a54567b14c31238b10138
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-24 01:28:43 -0600

    Update device-function-response.ts

--- src/responses/device-function-response.ts
+++ src/responses/device-function-response.ts
@@ -27,11 +27,19 @@ export interface DeviceFunctionValues{
     range: ValuesRange;
 }
 
-export interface DeviceFunctionResponse{
-    /** Class of the function */
-    functionClass: string;
-    /** Instance name of the function */
-    functionInstance: string;
-    /** Function values */
-    values: DeviceFunctionValues[];
+// Ensure the DeviceResponse interface is exported
+export interface DeviceResponse {
+  id: string;
+  deviceId: string;
+  children: DeviceResponse[];
+  typeId: string;
+  friendlyName: string;
+  description: {
+    device: {
+      manufacturerName: string;
+      model: string;
+      deviceClass: string;
+    };
+    functions: DeviceFunctionResponse[];
+  };
 }
