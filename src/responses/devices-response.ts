bc3caf688994fcae494af70dd079c5a7255bab85
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-24 08:14:06 -0600

    Update devices-response.ts

--- src/responses/devices-response.ts
+++ src/responses/devices-response.ts
@@ -1,20 +1,18 @@
-import { DeviceFunctionResponse } from './device-function-response';
+// src/responses/device-function-response.ts
+import { DeviceFunctionResponse } from './device-functions';
 
-/**
- * HTTP response for device discovery
- */
-export interface DeviceResponse{
-    id: string;
-    deviceId: string;
-    children: DeviceResponse[];
-    typeId: string;
-    friendlyName: string;
-    description: {
-        device: {
-            manufacturerName: string;
-            model: string;
-            deviceClass: string;
-        };
-        functions: DeviceFunctionResponse[];
+export interface DeviceResponse {
+  id: string;
+  deviceId: string;
+  children: DeviceResponse[];
+  typeId: string;
+  friendlyName: string;
+  description: {
+    device: {
+      manufacturerName: string;
+      model: string;
+      deviceClass: string;
     };
+    functions: DeviceFunctionResponse[];  // Now works correctly
+  };
 }
