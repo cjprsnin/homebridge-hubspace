3a62cc96fc952e200ec044377f830f8d5e50008d
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-23 22:14:12 -0600

    Update outlet-accessory.ts

--- src/accessories/outlet-accessory.ts
+++ src/accessories/outlet-accessory.ts
@@ -4,46 +4,73 @@ import { HubspacePlatform } from '../platform';
 import { isNullOrUndefined } from '../utils';
 import { HubspaceAccessory } from './hubspace-accessory';
 
-export class OutletAccessory extends HubspaceAccessory{
+export class SurgeProtectorAccessory extends HubspaceAccessory {
 
-    /**
-     * Crates a new instance of the accessory
-     * @param platform Hubspace platform
-     * @param accessory Platform accessory
-     */
-    constructor(platform: HubspacePlatform, accessory: PlatformAccessory) {
-        super(platform, accessory, [platform.Service.Outlet]);
+  /**
+   * Creates a new instance of the accessory for a surge protector
+   * @param platform Hubspace platform
+   * @param accessory Platform accessory
+   */
+  constructor(platform: HubspacePlatform, accessory: PlatformAccessory) {
+    super(platform, accessory, []);
 
-        this.configurePower();
+    this.configureOutlets();
 
-        this.removeStaleServices();
+    this.removeStaleServices();
+  }
+
+  /**
+   * Configures all outlets on the surge protector.
+   */
+  private configureOutlets(): void {
+    const outletFunctions = this.device.functions.filter((func) =>
+      func.type === DeviceFunction.OutletPower
+    );
+
+    // Create and configure a service for each outlet
+    outletFunctions.forEach((func, index) => {
+      const outletService = this.addService(
+        this.platform.Service.Outlet,
+        `Outlet ${index + 1}`,
+        `outlet-${index + 1}`
+      );
+
+      outletService
+        .getCharacteristic(this.platform.Characteristic.On)
+        .onGet(() => this.getOutletPower(func))
+        .onSet((value) => this.setOutletPower(func, value));
+    });
+  }
+
+  /**
+   * Gets the power state for a specific outlet.
+   * @param func Device function definition for the outlet
+   */
+  private async getOutletPower(func: DeviceFunction): Promise<CharacteristicValue> {
+    const value = await this.deviceService.getValueAsBoolean(
+      this.device.deviceId,
+      func.values[0].deviceValues[0].key
+    );
+
+    if (isNullOrUndefined(value)) {
+      throw new this.platform.api.hap.HapStatusError(
+        this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+      );
     }
 
-    private configurePower(): void{
-        if(this.supportsFunction(DeviceFunction.OutletPower)){
-            this.services[0].getCharacteristic(this.platform.Characteristic.On)
-                .onGet(this.getOn.bind(this))
-                .onSet(this.setOn.bind(this));
-        }
-    }
+    return value!;
+  }
 
-    private async getOn(): Promise<CharacteristicValue>{
-        // Try to get the value
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower);
-        const value = await this.deviceService.getValueAsBoolean(this.device.deviceId, func.values[0].deviceValues[0].key);
-
-        // If the value is not defined then show 'Not Responding'
-        if(isNullOrUndefined(value)){
-            throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-        }
-
-        // Otherwise return the value
-        return value!;
-    }
-
-    private async setOn(value: CharacteristicValue): Promise<void>{
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower);
-        await this.deviceService.setValue(this.device.deviceId, func.values[0].deviceValues[0].key, value);
-    }
-
+  /**
+   * Sets the power state for a specific outlet.
+   * @param func Device function definition for the outlet
+   * @param value Desired state (on/off)
+   */
+  private async setOutletPower(func: DeviceFunction, value: CharacteristicValue): Promise<void> {
+    await this.deviceService.setValue(
+      this.device.deviceId,
+      func.values[0].deviceValues[0].key,
+      value
+    );
+  }
 }
