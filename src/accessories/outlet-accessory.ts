e5e93da515a3589e08945fe20db000611a499397
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-24 01:32:36 -0600

    Update outlet-accessory.ts

--- src/accessories/outlet-accessory.ts
+++ src/accessories/outlet-accessory.ts
@@ -49,31 +49,25 @@ export class OutletAccessory extends HubspaceAccessory {
 }
 
 export class SurgeProtectorAccessory extends HubspaceAccessory {
-  /**
-   * Creates a new instance of the accessory for a surge protector
-   * @param platform Hubspace platform
-   * @param accessory Platform accessory
-   */
   constructor(platform: HubspacePlatform, accessory: PlatformAccessory) {
     super(platform, accessory, []);
-
     this.configureOutlets();
-
     this.removeStaleServices();
   }
 
-  /**
-   * Configures all outlets on the surge protector.
-   */
   private configureOutlets(): void {
-    const outletFunctions = (this.device as DeviceResponse).description.functions.filter((func) =>
+    const outletFunctions = this.device.description.functions.filter((func) =>
       func.functionClass === DeviceFunction.OutletPower
     );
 
     // Create and configure a service for each outlet
     outletFunctions.forEach((func, index) => {
-      const outletService = this.addService(this.platform.Service.Outlet, `Outlet ${index + 1}`);
-      
+      const outletService = this.accessory.addService(
+        this.platform.Service.Outlet,
+        `Outlet ${index + 1}`,
+        `outlet-${index + 1}`
+      );
+
       outletService
         .getCharacteristic(this.platform.Characteristic.On)
         .onGet(() => this.getOutletPower(func))
@@ -81,11 +75,7 @@ export class SurgeProtectorAccessory extends HubspaceAccessory {
     });
   }
 
-  /**
-   * Gets the power state for a specific outlet.
-   * @param func Device function definition for the outlet
-   */
-  private async getOutletPower(func: DeviceFunctionResponse): Promise<CharacteristicValue> {
+  private async getOutletPower(func: DeviceFunction): Promise<CharacteristicValue> {
     const value = await this.deviceService.getValueAsBoolean(
       this.device.deviceId,
       func.values[0].deviceValues[0].key
@@ -100,12 +90,7 @@ export class SurgeProtectorAccessory extends HubspaceAccessory {
     return value!;
   }
 
-  /**
-   * Sets the power state for a specific outlet.
-   * @param func Device function definition for the outlet
-   * @param value Desired state (on/off)
-   */
-  private async setOutletPower(func: DeviceFunctionResponse, value: CharacteristicValue): Promise<void> {
+  private async setOutletPower(func: DeviceFunction, value: CharacteristicValue): Promise<void> {
     await this.deviceService.setValue(
       this.device.deviceId,
       func.values[0].deviceValues[0].key,
