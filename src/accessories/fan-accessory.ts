de213f6ad530a26cd86e0a09e47f318a707fe7df
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-25 11:06:07 -0600

    Update fan-accessory.ts

--- src/accessories/fan-accessory.ts
+++ src/accessories/fan-accessory.ts
@@ -1,81 +1,31 @@
-import { CharacteristicValue, PlatformAccessory } from 'homebridge';
-import { HubspacePlatform } from '../platform';
-import { HubspaceAccessory } from './hubspace-accessory';
-import { isNullOrUndefined } from '../utils';
-import { DeviceFunction, getDeviceFunctionDef } from '../models/device-functions';
+private async setActive(value: CharacteristicValue): Promise<void> {
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
+    await this.deviceService.setValue(this.device.deviceId, func.deviceValues[0].key, value);
+}
 
-/**
- * Fan accessory for Hubspace platform
- */
-export class FanAccessory extends HubspaceAccessory{
+private async getActive(): Promise<CharacteristicValue> {
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
+    const value = await this.deviceService.getValue(this.device.deviceId, func.deviceValues[0].key);
 
-    /**
-     * Crates a new instance of the accessory
-     * @param platform Hubspace platform
-     * @param accessory Platform accessory
-     */
-    constructor(platform: HubspacePlatform, accessory: PlatformAccessory) {
-        super(platform, accessory, [platform.Service.Fanv2]);
-
-        this.configureActive();
-        this.configureRotationSpeed();
-
-        this.removeStaleServices();
+    if (isNullOrUndefined(value)) {
+        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
 
-    private configureActive(): void{
-        this.services[0].getCharacteristic(this.platform.Characteristic.Active)
-            .onGet(this.getActive.bind(this))
-            .onSet(this.setActive.bind(this));
+    return value!;
+}
+
+private async getRotationSpeed(): Promise<CharacteristicValue> {
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
+    const value = await this.deviceService.getValue(this.device.deviceId, func.deviceValues[0].key);
+
+    if (isNullOrUndefined(value)) {
+        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
 
-    private configureRotationSpeed(): void{
-        this.services[0].getCharacteristic(this.platform.Characteristic.RotationSpeed)
-            .onGet(this.getRotationSpeed.bind(this))
-            .onSet(this.setRotationSpeed.bind(this))
-            .setProps({
-                minValue: 0,
-                maxValue: 100,
-                minStep: 25
-            });
-    }
+    return value!;
+}
 
-    private async setActive(value: CharacteristicValue): Promise<void>{
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
-        await this.deviceService.setValue(this.device.deviceId, func.values[0].deviceValues[0].key, value);
-    }
-
-    private async getActive(): Promise<CharacteristicValue>{
-        // Try to get the value
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
-        const value = await this.deviceService.getValue(this.device.deviceId, func.values[0].deviceValues[0].key);
-
-        // If the value is not defined then show 'Not Responding'
-        if(isNullOrUndefined(value)){
-            throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-        }
-
-        // Otherwise return the value
-        return value!;
-    }
-
-    private async getRotationSpeed(): Promise<CharacteristicValue>{
-        // Try to get the value
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
-        const value = await this.deviceService.getValue(this.device.deviceId, func.values[0].deviceValues[0].key);
-
-        // If the value is not defined then show 'Not Responding'
-        if(isNullOrUndefined(value)){
-            throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-        }
-
-        // Otherwise return the value
-        return value!;
-    }
-
-    private async setRotationSpeed(value: CharacteristicValue): Promise<void>{
-        const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
-        await this.deviceService.setValue(this.device.deviceId, func.values[0].deviceValues[0].key, value);
-    }
-
+private async setRotationSpeed(value: CharacteristicValue): Promise<void> {
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
+    await this.deviceService.setValue(this.device.deviceId, func.deviceValues[0].key, value);
 }
