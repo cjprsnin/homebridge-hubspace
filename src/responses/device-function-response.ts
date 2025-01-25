41d06b12044abd6e107cba1915a7cdff4f1af437
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-25 13:50:13 -0600

    Update device-function-response.ts

--- src/responses/device-function-response.ts
+++ src/responses/device-function-response.ts
@@ -1,23 +1,24 @@
-// src/responses/device-function-response.ts
+/**
+ * Response for device function
+ */
 
-// Assuming DeviceValues, ValuesRange, and DeviceFunctionValues are defined as per your earlier structure
-export interface DeviceValues {
+export interface DeviceValues{
   /** Device values */
   type: string;
-  /** Key ID */
+  /** key id */
   key: string;
 }
 
-export interface ValuesRange {
-  /** Minimum value */
+export interface ValuesRange{
+  /** miniumum value */
   min: number;
-  /** Maximum value */
+  /** maximum value */
   max: number;
-  /** Step value */
+  /** step value */
   step: number;
 }
 
-export interface DeviceFunctionValues {
+export interface DeviceFunctionValues{
   /** Name of the value */
   name: string;
   /** Possible values */
@@ -26,28 +27,11 @@ export interface DeviceFunctionValues {
   range: ValuesRange;
 }
 
-// DeviceFunctionResponse definition
-export interface DeviceFunctionResponse {
-  values: any;
-  functionInstance: import("/vscode-vfs/github+7b2276223a312c22726566223a7b2274797065223a352c226964223a226d6173746572227d7d/cjprsnin/homebridge-hubspace/src/models/device-functions").DeviceFunction;
+export interface DeviceFunctionResponse{
+  /** Class of the function */
   functionClass: string;
-  functionInstanceName: string;
-  deviceValues: DeviceValues[]; // Add this property
-}
-
-// DeviceResponse definition
-export interface DeviceResponse {
-  id: string;
-  deviceId: string;
-  children: DeviceResponse[];
-  typeId: string;
-  friendlyName: string;
-  description: {
-    device: {
-      manufacturerName: string;
-      model: string;
-      deviceClass: string;
-    };
-    functions: DeviceFunctionResponse[];  // Corrected usage of DeviceFunctionResponse here
-  };
+  /** Instance name of the function */
+  functionInstance: string;
+  /** Function values */
+  values: DeviceFunctionValues[];
 }
