00d38d082ece4fa8696a525bf7836f2198bdc8ac
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-25 11:07:23 -0600

    Update fan-accessory.ts

--- src/accessories/fan-accessory.ts
+++ src/accessories/fan-accessory.ts
@@ -1,31 +1,36 @@
-private async setActive(value: CharacteristicValue): Promise<void> {
+class FanAccessory extends HubspaceAccessory {
+  constructor(platform: HubspacePlatform, accessory: PlatformAccessory) {
+    super(platform, accessory, [platform.Service.Fanv2]);
+    this.configureActive();
+    this.configureRotationSpeed();
+    this.removeStaleServices();
+  }
+
+  private async setActive(value: CharacteristicValue): Promise<void> {
     const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
     await this.deviceService.setValue(this.device.deviceId, func.deviceValues[0].key, value);
-}
+  }
 
-private async getActive(): Promise<CharacteristicValue> {
+  private async getActive(): Promise<CharacteristicValue> {
     const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanPower);
     const value = await this.deviceService.getValue(this.device.deviceId, func.deviceValues[0].key);
-
     if (isNullOrUndefined(value)) {
-        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
-
     return value!;
-}
+  }
 
-private async getRotationSpeed(): Promise<CharacteristicValue> {
+  private async getRotationSpeed(): Promise<CharacteristicValue> {
     const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
     const value = await this.deviceService.getValue(this.device.deviceId, func.deviceValues[0].key);
-
     if (isNullOrUndefined(value)) {
-        throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
     }
-
     return value!;
-}
+  }
 
-private async setRotationSpeed(value: CharacteristicValue): Promise<void> {
+  private async setRotationSpeed(value: CharacteristicValue): Promise<void> {
     const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.FanSpeed);
     await this.deviceService.setValue(this.device.deviceId, func.deviceValues[0].key, value);
+  }
 }
