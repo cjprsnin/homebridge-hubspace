a851ca6258996d77fb8d6cc4def95379a1713874
gym <cjprsnin@comcast.net>
2025-01-25 21:51:13 -0600

    nbmbnm

--- src/services/discovery.service.ts
+++ src/services/discovery.service.ts
@@ -159,15 +159,15 @@ export class DiscoveryService {
             platformAccessory.context = { device: parentDevice };
   
             const switchService = platformAccessory.addService(
-                new hap.Service.Switch(parentDevice.name, parentDevice.uuid)
+                new hap.Service.Switch(parentDevice.name, parentDevice.uuid) as hap.Service
             );
             
             switchService
-                .getCharacteristic(hap.Characteristic.On)
+                .getCharacteristic(hap.Characteristic.On as hap.Characteristic)
                 .on('set', (value, callback) => {
                     console.log(`Switch toggled to ${value}`);
                     callback();
-                });        
+                });       
   
           this._platform.api.publishExternalAccessories('homebridge-hubspace', [platformAccessory]);
           this._platform.log.info(`Parent device created for ${parentDevice.name}:`, parentDevice);
