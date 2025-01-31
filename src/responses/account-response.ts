9842cd3407a7f8f4e1da556bb94e286c3c7a91df
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-31 16:43:28 -0600

    Update account-response.ts

--- src/responses/account-response.ts
+++ src/responses/account-response.ts
@@ -1,21 +1,10 @@
-public async loadAccount(): Promise<void> {
-    try {
-        // Make a GET request to the /users/me endpoint
-        const response = await this._client.get<AccountResponse>('/users/me');
-
-        // Extract the account ID from the response
-        this._accountId = response.data.accountAccess[0].account.accountId;
-
-        // Invoke the callback if set
-        if (this._onAccountLoaded) {
-            await this._onAccountLoaded();
-        }
-    } catch (ex) {
-        const axiosError = <AxiosError>ex;
-        const friendlyMessage =
-            axiosError.response?.status === 401 ? 'Incorrect username or password' : axiosError.message;
-
-        this._log.error('Failed to load account information.', friendlyMessage);
-        throw axiosError;
-    }
+/**
+ * HTTP response with account details
+ */
+export interface AccountResponse {
+  accountAccess: {
+    account: {
+      accountId: string;
+    };
+  }[];
 }
