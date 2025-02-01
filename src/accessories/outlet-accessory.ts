a1caf5456c87a43a2b4904bb3f9c861d5f336a34
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-31 22:25:18 -0600

    Update outlet-accessory.ts

--- src/accessories/outlet-accessory.ts
+++ src/accessories/outlet-accessory.ts
@@ -1,7 +1,7 @@
 import { PlatformAccessory, CharacteristicValue, Service } from 'homebridge';
 import { HubspacePlatform } from '../platform';
 import { Device} from '../models/device';
-import { DeviceFunction, DeviceFunctionDef  } from '../models/device-functions';
+import { DeviceFunction, getDeviceFunctionDef  } from '../models/device-functions';
 import { AdditionalData } from './device-accessory-factory';
 import { HubspaceAccessory } from './hubspace-accessory';
 
@@ -40,7 +40,7 @@ export class OutletAccessory extends HubspaceAccessory {
   }
 
   private async getOn(): Promise<CharacteristicValue> {
-    const func = DeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
     const value = await this.deviceService.getValueAsBoolean(this.device.deviceId, func.deviceValues[this.outletIndex].key);
     this.log.debug(`${this.device.name}: Received ${value} from Hubspace Power`);
     return value ?? false; // Ensure a boolean is returned
@@ -48,7 +48,7 @@ export class OutletAccessory extends HubspaceAccessory {
 
   private async setOn(value: CharacteristicValue): Promise<void> {
     this.log.debug(`${this.device.name}: Received ${value} from Homekit Power`);
-    const func = DeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
     await this.deviceService.setValue(this.device.deviceId, func.deviceValues[this.outletIndex].key, value);
   }
 
