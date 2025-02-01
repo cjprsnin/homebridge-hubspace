410b5e76f9f52c598c6de99d3e2fb32404520d06
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-01 01:44:04 -0600

    Update device-functions.ts

--- src/models/device-functions.ts
+++ src/models/device-functions.ts
@@ -26,36 +26,6 @@ export enum DeviceFunction {
   Spigot2 = 'spigot-2',
 }
 
-export interface DeviceValues {
-  type: string;
-  key: string;
-  values: DeviceFunctionValues[];
-}
-
-export interface ValuesRange {
-  min: number;
-  max: number;
-  step: number;
-}
-
-export interface DeviceFunctionValues {
-  name: string;
-  deviceValues: DeviceValues[];
-  range: ValuesRange;
-}
-
-export interface DeviceFunctionResponse {
-  functionClass: string;
-  functionInstance: string;
-  values: DeviceFunctionValues[];
-  outletIndex?: number;
-}
-
-export interface DeviceFunctionDef {
-  functionClass: DeviceFunction;
-  functionInstanceName: string;
-}
-
 export const DeviceFunctions: DeviceFunctionDef[] = [
   {
     functionClass: DeviceFunction.Power,
