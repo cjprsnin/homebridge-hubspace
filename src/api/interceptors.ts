31270c993981ca515c32a6d92b7bc8c1089c421a
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-31 23:33:56 -0600

    Update interceptors.ts

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,5 +1,5 @@
 import { InternalAxiosRequestConfig } from 'axios';
-import { TTokenResponse } from '../services/token.service';
+import { TokenService } from './services/token.service';
 
 /**
  * Adds a Bearer token to the request
@@ -7,7 +7,7 @@ import { TTokenResponse } from '../services/token.service';
  * @returns Config with Bearer token
  */
 export async function addBearerToken(config: InternalAxiosRequestConfig<unknown>): Promise<InternalAxiosRequestConfig<unknown>>{
-    const token = await TokenResponse.instance.getToken();
+    const token = await TokenService.instance.getToken();
 
     if(token){
         config.headers.Authorization = `Bearer ${token}`;
