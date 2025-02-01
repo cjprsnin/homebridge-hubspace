9d7e1f2ad0c26436d4bf673b4a9f99f1b2bf0a0e
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-01 01:40:01 -0600

    Update device-function-def.ts

--- src/models/device-function-def.ts
+++ src/models/device-function-def.ts
@@ -7,15 +7,3 @@ export interface DeviceFunctionDef {
   functionInstanceName: string;
 }
 
-
-// Example of a power function definition
-const powerFunction: DeviceFunctionDef = {
-  functionClass: DeviceFunction.Power,
-  functionInstanceName: 'fan-power',
-};
-
-// Example of a brightness function definition
-const brightnessFunction: DeviceFunctionDef = {
-  functionClass: DeviceFunction.Brightness,
-  functionInstanceName: 'default-brightness',
-};
