8587e05478347efdc82a54a9712b485e4fcdd3d5
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-31 23:21:00 -0600

    Update token.service.ts

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,46 +1,26 @@
-export class TokenService {
-  private static _instance: TokenService;
-  private token: string | null = null;
-  private tokenExpiration: number | null = null;
+/**
+ * Auth token response from HTTP server
+ */
+export interface TokenResponse{
 
-  private constructor() {}
+    /**
+     * Auth access token
+     */
+    access_token: string;
 
-  public static init(username: string, password: string): void {
-    if (!TokenService._instance) {
-      TokenService._instance = new TokenService();
-    }
-    // Perform login and set the token
-  }
+    /**
+     * Access token expiration in seconds
+     */
+    expires_in: number;
 
-  public static get instance(): TokenService {
-    if (!TokenService._instance) {
-      throw new Error('TokenService has not been initialized.');
-    }
-    return TokenService._instance;
-  }
+    /**
+     * Refresh token expiration in seconds
+     */
+    refresh_expires_in: number;
 
-  public setToken(token: string, expiresIn?: number): void {
-    this.token = token;
-    if (expiresIn) {
-      this.tokenExpiration = Date.now() + expiresIn * 1000; // Convert to milliseconds
-    } else {
-      this.tokenExpiration = null;
-    }
-  }
+    /**
+     * Refresh token
+     */
+    refresh_token: string;
 
-  public getToken(): string | null {
-    if (this.tokenExpiration && Date.now() >= this.tokenExpiration) {
-      this.clearToken(); // Token has expired
-    }
-    return this.token;
-  }
-
-  public clearToken(): void {
-    this.token = null;
-    this.tokenExpiration = null;
-  }
-
-  public isTokenValid(): boolean {
-    return !!this.token && (!this.tokenExpiration || Date.now() < this.tokenExpiration);
-  }
 }
