8594d18ed34fb2de152a65c376ccaac01fde5959
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-01-31 19:27:23 -0600

    Update outlet-accessory.ts

--- src/accessories/outlet-accessory.ts
+++ src/accessories/outlet-accessory.ts
@@ -2,11 +2,13 @@ import { PlatformAccessory, CharacteristicValue, Service } from 'homebridge';
 import { HubspacePlatform } from '../platform';
 import { Device, DeviceFunction, getDeviceFunctionDef } from '../models/device'; // Correct import path
 import { AdditionalData } from './device-accessory-factory'; // Import AdditionalData
+import { HubspaceAccessory } from './hubspace-accessory'; // Import HubspaceAccessory interface
 
 export class OutletAccessory implements HubspaceAccessory {
-  public services: Service[] = []; // Add required properties
-  public log = this.platform.log;
-  public config = this.platform.config;
+  public services: Service[] = []; // List of services provided by the accessory
+  public log = this.platform.log; // Logger instance
+  public config = this.platform.config; // Configuration object
+  public deviceService = this.platform.deviceService; // Device service instance
 
   constructor(
     private readonly platform: HubspacePlatform,
@@ -15,30 +17,91 @@ export class OutletAccessory implements HubspaceAccessory {
     private readonly outletIndex: number,
     private readonly additionalData?: AdditionalData
   ) {
-    this.configureAccessory();
+    this.initializeService();
+    this.setAccessoryInformation();
   }
 
-  private configureAccessory(): void {
+  /**
+   * Initializes the service for the outlet accessory.
+   */
+  public initializeService(): void {
     const service = this.accessory.getService(this.platform.Service.Outlet) || this.accessory.addService(this.platform.Service.Outlet);
     this.services.push(service);
 
     service.getCharacteristic(this.platform.Characteristic.On)
-      .onGet(this.getOn.bind(this))
-      .onSet(this.setOn.bind(this));
+      .onGet(() => this.getOn())
+      .onSet((value) => this.setOn(value));
+
+    this.configureName(service, this.device.name);
   }
 
- private async getOn(): Promise<CharacteristicValue> {
-  const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
-  const value = await this.platform.deviceService.getValueAsBoolean(this.device.deviceId, func.deviceValues[this.outletIndex].key);
-  return value ?? false; // Ensure a boolean is returned
-}
+  /**
+   * Sets the accessory information (Manufacturer, Model, SerialNumber).
+   */
+  public setAccessoryInformation(): void {
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, this.device.manufacturer ?? 'N/A')
+      .setCharacteristic(this.platform.Characteristic.Model, this.device.model.join(', ') ?? 'N/A')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.device.deviceId ?? 'N/A');
+  }
 
-  private async setOn(value: CharacteristicValue): Promise<void> {
+  /**
+   * Determines if the given device function is supported by the accessory.
+   * @param functionName The name of the device function.
+   * @returns True if the function is supported, otherwise false.
+   */
+  public supportsFunction(functionName: string): boolean {
+    return this.device.functions.some(f => f.name === functionName);
+  }
+
+  /**
+   * Updates the state of the accessory.
+   * @param state The new state of the accessory.
+   */
+  public updateState(state: any): void {
+    const service = this.services[0];
+    if (service) {
+      service.updateCharacteristic(this.platform.Characteristic.On, state.power ?? false);
+    }
+  }
+
+  /**
+   * Gets the power state of the outlet.
+   * @returns The power state of the outlet.
+   */
+  private async getOn(): Promise<CharacteristicValue> {
     const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
-    await this.platform.deviceService.setValue(this.device.deviceId, func.deviceValues[this.outletIndex].key, value);
+    const value = await this.deviceService.getValueAsBoolean(this.device.deviceId, func.deviceValues[this.outletIndex].key);
+    this.log.debug(`${this.device.name}: Received ${value} from Hubspace Power`);
+    return value ?? false; // Ensure a boolean is returned
   }
-  
+
+  /**
+   * Sets the power state of the outlet.
+   * @param value The new power state.
+   */
+  private async setOn(value: CharacteristicValue): Promise<void> {
+    this.log.debug(`${this.device.name}: Received ${value} from Homekit Power`);
+    const func = getDeviceFunctionDef(this.device.functions, DeviceFunction.OutletPower, undefined, this.outletIndex);
+    await this.deviceService.setValue(this.device.deviceId, func.deviceValues[this.outletIndex].key, value);
+  }
+
+  /**
+   * Configures the name for a service.
+   * @param service The service to configure.
+   * @param name The name to set.
+   */
+  private configureName(service: Service, name: string): void {
+    service.setCharacteristic(this.platform.Characteristic.Name, name);
+    service.setCharacteristic(this.platform.Characteristic.ConfiguredName, name);
+    this.log.info(`Configured service name: ${name}`);
+  }
+
+  /**
+   * Returns the list of services provided by the accessory.
+   * @returns The list of services.
+   */
   public getServices(): Service[] {
-  return this.services;
-}
+    return this.services;
+  }
 }
