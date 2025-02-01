434adeb82c92264a1d05fd52b4e4e7811d8ad619
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-01 01:00:49 -0600

    Update device-function-response.ts

--- src/responses/device-function-response.ts
+++ src/responses/device-function-response.ts
@@ -1,41 +1,24 @@
-/**
- * Response for device function
- */
-
-export interface DeviceValues{
-    /** Device values */
-    type: string;
-    /** key id */
-    key: string;
-      /** Function values */
-      values: DeviceFunctionValues[];
+export interface DeviceValues {
+  type: string;
+  key: string;
+  values: DeviceFunctionValues[];
 }
 
-export interface ValuesRange{
-    /** miniumum value */
-    min: number;
-    /** maximum value */
-    max: number;
-    /** step value */
-    step: number;
+export interface ValuesRange {
+  min: number;
+  max: number;
+  step: number;
 }
 
-export interface DeviceFunctionValues{
-    /** Name of the value */
-    name: string;
-    /** Possible values */
-    deviceValues: DeviceValues[];
-    /** Range of Values */
-    range: ValuesRange;
+export interface DeviceFunctionValues {
+  name: string;
+  deviceValues: DeviceValues[];
+  range: ValuesRange;
 }
 
-export interface DeviceFunctionResponse{
-    /** Class of the function */
-    functionClass: string;
-    /** Instance name of the function */
-    functionInstance: string;
-    /** Function values */
-    values: DeviceFunctionValues[];
-    /** Outlet index (if applicable) */
-    outletIndex?: number;
+export interface DeviceFunctionResponse {
+  functionClass: string;
+  functionInstance: string;
+  values: DeviceFunctionValues[];
+  outletIndex?: number;
 }
