d3c2a27a5ef84840fe755433cb7b2c1150ac3d3c
cjprsnin <46409875+cjprsnin@users.noreply.github.com>
2025-02-01 20:05:29 -0600

    Update fan-accessory.ts

--- src/accessories/fan-accessory.ts
+++ src/accessories/fan-accessory.ts
@@ -1,12 +1,11 @@
 import { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
 import { HubspacePlatform } from '../platform';
-import { HubspaceAccessory } from './hubspace-accessory';
 import { Device } from '../models/device';
 import { AdditionalData } from './device-accessory-factory';
 import { isNullOrUndefined } from '../utils';
 import { DeviceFunction, getDeviceFunctionDef } from '../models/device-functions';
-import { AdditionalData } from '../models/additional-data';
-
+import { AdditionalData } from '../models/additional-data'; // Correct import path
+import { HubspaceAccessory } from './hubspace-accessory';
 
 /**
  * Fan accessory for Hubspace platform
